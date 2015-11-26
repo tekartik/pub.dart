5adf41743cfda9b99e47b819e830165ffda8deb4
Alexandre Roux Tekartik <alex@tekartik.com>
2015-11-26 12:42:27 +0100

    feat: initial revision

---
+++ lib/pub.dart
@@ -0,0 +1,135 @@
+library tekartik_io_tools.pub_utils;
+
+import 'package:cmdo/dartbin.dart' as _dartbin;
+import 'package:cmdo/cmdo.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path/path.dart';
+
+bool _DEBUG = false;
+
+enum TestReporter { COMPACT, EXPANDED }
+
+final Map<String, TestReporter> _testReporterMap = new Map.fromIterables(
+    ["compact", "expanded"], [TestReporter.COMPACT, TestReporter.EXPANDED]);
+
+final Map<TestReporter, String> _testReporterStringMap =
+    new Map.fromIterables(_testReporterMap.values, _testReporterMap.keys);
+
+String testReporterString(TestReporter reporter) =>
+    _testReporterStringMap[reporter];
+
+List<String> testReporterStrings = new List.from(_testReporterStringMap.values);
+TestReporter testReporterFromString(String reporterString) =>
+    _testReporterMap[reporterString];
+
+///
+/// A local pub package
+///
+class PubPackage {
+  String _path;
+
+  String get path => _path;
+
+  PubPackage(this._path);
+
+  CommandInput pubCmd(List<String> args) =>
+      _dartbin.pubCmd(args)..workingDirectory = _path;
+
+  CommandInput upgradeCmd(List<String> args) {
+    args = new List.from(args);
+    args.insertAll(0, ['upgrade']);
+    return pubCmd(args);
+  }
+
+  CommandInput runTestCmd(List<String> args,
+      {TestReporter reporter,
+      bool color,
+      int concurrency,
+      List<String> platforms,
+      String name}) {
+    args = new List.from(args);
+    args.insertAll(0, ['run', 'test']);
+    if (reporter != null) {
+      args.addAll(['-r', testReporterString(reporter)]);
+    }
+    if (concurrency != null) {
+      args.addAll(['-j', concurrency.toString()]);
+    }
+    if (name != null) {
+      args.addAll(['-n', name]);
+    }
+    if (color != null) {
+      if (color) {
+        args.add('--color');
+      } else {
+        args.add('--no-color');
+      }
+    }
+    if (platforms != null) {
+      for (String platform in platforms) {
+        args.addAll(['-p', platform]);
+      }
+    }
+    return pubCmd(args);
+  }
+
+  // same package is same path
+
+  @override
+  int get hashCode => path.hashCode;
+
+  @override
+  bool operator ==(o) => path == o.path;
+
+  @override
+  String toString() => path;
+}
+
+final String _pubspecYaml = "pubspec.yaml";
+
+/// return true if root package
+Future<bool> isPubPackageRoot(String dirPath) async {
+  String pubspecYamlPath = join(dirPath, _pubspecYaml);
+  return await FileSystemEntity.isFile(pubspecYamlPath);
+}
+
+bool isPubPackageRootSync(String dirPath) {
+  String pubspecYamlPath = join(dirPath, _pubspecYaml);
+  return FileSystemEntity.isFileSync(pubspecYamlPath);
+}
+
+/// throws if no project found
+Future<String> getPubPackageRoot(String resolverPath) async {
+  String dirPath = normalize(absolute(resolverPath));
+
+  while (true) {
+    // Find the project root path
+    if (await isPubPackageRoot(dirPath)) {
+      return dirPath;
+    }
+    String parentDirPath = dirname(dirPath);
+
+    if (parentDirPath == dirPath) {
+      throw new Exception("No project found for path '$resolverPath");
+    }
+    dirPath = parentDirPath;
+  }
+}
+
+String getPubPackageRootSync(String resolverPath) {
+  String dirPath = normalize(absolute(resolverPath));
+
+  while (true) {
+    // Find the project root path
+    if (isPubPackageRootSync(dirPath)) {
+      return dirPath;
+    }
+    String parentDirPath = dirname(dirPath);
+
+    if (parentDirPath == dirPath) {
+      throw new Exception("No project found for path '$resolverPath");
+    }
+    dirPath = parentDirPath;
+  }
+}
---
+++ test/pub_test.dart
@@ -0,0 +1,62 @@
+@TestOn("vm")
+library tekartik_pub.test.pub_test;
+
+import 'dart:mirrors';
+import 'package:path/path.dart';
+import 'package:cmdo/cmdo_io.dart';
+import 'package:cmdo/dartbin.dart';
+import 'package:dev_test/test.dart';
+import 'package:tekartik_pub/pub.dart';
+import 'dart:async';
+
+class _TestUtils {
+  static final String scriptPath =
+      (reflectClass(_TestUtils).owner as LibraryMirror).uri.toFilePath();
+}
+
+String get testScriptPath => _TestUtils.scriptPath;
+
+void main() => defineTests();
+
+Future<String> get _pubPackageRoot => getPubPackageRoot(testScriptPath);
+
+void defineTests() {
+  //useVMConfiguration();
+  group('pub', () {
+    test('version', () async {
+      CommandResult result = await io.runCmd(pubCmd(['--version']));
+      expect(result.out.startsWith("Pub"), isTrue);
+    });
+
+    _testIsPubPackageRoot(String path, bool expected) async {
+      expect(await isPubPackageRoot(path), expected);
+      expect(isPubPackageRootSync(path), expected);
+    }
+
+    test('root', () async {
+      _testIsPubPackageRoot(dirname(testScriptPath), false);
+      _testIsPubPackageRoot(dirname(dirname(dirname(testScriptPath))), false);
+      _testIsPubPackageRoot(dirname(dirname(testScriptPath)), true);
+      expect(await _pubPackageRoot, dirname(dirname(testScriptPath)));
+      try {
+        await getPubPackageRoot(join('/', 'dummy', 'path'));
+        fail('no');
+      } catch (e) {}
+    });
+
+    group('pub_package', () {
+      test('runTest', () async {
+        PubPackage pkg = new PubPackage(await _pubPackageRoot);
+        CommandResult result = await io.runCmd(pkg.runTestCmd(
+            ['test/data/success_test_.dart'],
+            platforms: ["vm"],
+            reporter: TestReporter.EXPANDED,
+            concurrency: 1));
+
+        expect(result.exitCode, 0);
+        result = await io.runCmd(pkg.runTestCmd(['test/data/fail_test_.dart']));
+        expect(result.exitCode, 1);
+      });
+    });
+  });
+}
