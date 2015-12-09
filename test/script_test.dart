521f861fe807f3ec7c776d16f5eb5c355e5b2bc8
Alexandre Roux Tekartik <alex@tekartik.com>
2015-12-09 16:52:15 +0100

    test: TestScript mirror helper

--- test/script_test.dart
+++ test/script_test.dart
@@ -4,6 +4,7 @@ library tekartik_pub.test.pub_test;
 import 'dart:mirrors';
 import 'package:dev_test/test.dart';
 import 'package:tekartik_pub/script.dart';
+import 'package:path/path.dart';
 
 class _TestUtils {
   static final String scriptPath =
@@ -12,12 +13,17 @@ class _TestUtils {
 
 String get testScriptPath => _TestUtils.scriptPath;
 
+// This script resolver
 class TestScript extends Script {}
+
+// Test directory
+String get testDirPath => dirname(getScriptPath(TestScript));
 
 void main() {
   //useVMConfiguration();
   test('script', () async {
     expect(getScriptPath(TestScript), testScriptPath);
     expect(getScriptPath(TestScript), new TestScript().path);
+    expect(testDirPath, dirname(testScriptPath));
   });
 }
