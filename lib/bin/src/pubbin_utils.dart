0990da36e13876d8a242aa25e1fd43ce6bce3657
Alexandre Roux <alex@tekartik.com>
2018-12-14 14:39:08 +0100

    test: fix travis

--- lib/bin/src/pubbin_utils.dart
+++ lib/bin/src/pubbin_utils.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'dart:async';
 import 'package:args/args.dart';
 import 'package:process_run/cmd_run.dart' as cmd_run;
 import 'package:process_run/cmd_run.dart' hide runCmd;
